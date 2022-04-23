ce50bc34efc4a648ff7ec06051223a1fbc1469b1
Jiyeon Song <jyeon3070@gmail.com>
2022-04-23 23:26:54 +0900

    feature: 예제 만들기
    - AppConfig 리팩터링

--- src/main/java/com/spring/core/AppConfig.java
+++ src/main/java/com/spring/core/AppConfig.java
@@ -1,6 +1,9 @@
 package com.spring.core;
 
+import com.spring.core.discount.DiscountPolicy;
 import com.spring.core.discount.FixDiscountPolicy;
+import com.spring.core.discount.RateDiscountPolicy;
+import com.spring.core.member.MemberRepository;
 import com.spring.core.member.MemberService;
 import com.spring.core.member.MemberServiceImpl;
 import com.spring.core.member.MemoryMemberRepository;
@@ -9,11 +12,19 @@ import com.spring.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
